8e37847f54966097d2ef215e5ebd87acbee7c97c
Badi Labassi <badi.labassi@gmail.com>
2018-10-21 01:11:36 -0400

    Improve Page speed loading

--- src/html.js
+++ src/html.js
@@ -1,18 +1,5 @@
 import React from 'react';
-// import { TypographyStyle } from "react-typography"
 import * as PropTypes from 'prop-types';
-// import typography from "./utils/typography"
-
-// import '@fortawsome/font-awesome/css/font-awesome.min.css';
-
-// let stylesStr;
-// if (process.env.NODE_ENV === 'production') {
-//   try {
-//     stylesStr = require('!raw!../public/styles.css');
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
 
 const propTypes = {
   headComponents: PropTypes.node.isRequired,
@@ -23,15 +10,6 @@ const propTypes = {
 class Html extends React.Component {
   render() {
     const { headComponents, body, postBodyComponents } = this.props;
-    // let css;
-    // if (process.env.NODE_ENV === 'production') {
-    //   css = (
-    //     <style
-    //       id="gatsby-inlined-css"
-    //       dangerouslySetInnerHTML={{ __html: stylesStr }}
-    //     />
-    //   );
-    // }
 
     return (
       <html>
@@ -73,21 +51,16 @@ class Html extends React.Component {
           />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          {/* {process.env.NODE_ENV === 'production' ? (
-            <link href="/styles.css" rel="stylesheet" />
-          ) : null} */}
-          {/* <link
-            href="//use.fontawesome.com/releases/v5.0.10/css/all.css"
-            rel="stylesheet"
-          /> */}
-          <script
-            type="text/javascript"
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0GE3dFR0orMQ7l8PJKtRU_3p99pgbrmw"
-          />
         </head>
         <body>
           <div id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />
           {postBodyComponents}
+          <script
+            type="text/javascript"
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0GE3dFR0orMQ7l8PJKtRU_3p99pgbrmw"
+            async
+            defer
+          />
         </body>
       </html>
     );
